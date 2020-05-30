83459ba14275abfcde9cd3689ebb03c472359e6a
Kristian Schlikow <kristian@schlikow.de>
2020-05-30 12:16:55 +0200

    Annotations and Prepare for Transpiler

--- Source/DSGUI/DSGUI_Harmony.cs
+++ Source/DSGUI/DSGUI_Harmony.cs
@@ -17,17 +17,13 @@ namespace DSGUI
         static HarmonyPatches()
         {
             var harmony = new Harmony("net.netrve.dsgui");
-
-            if (DSGUIMod.settings.DSGUI_UseTranspiler)
-                harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap), "AddHumanlikeOrders"),
-                    transpiler: new HarmonyMethod(typeof(Patch_AddHumanlikeOrders), nameof(Patch_AddHumanlikeOrders.Transpiler)));
             
             // We patch all as we use annotations
             harmony.PatchAll();
         }
         
         [HarmonyPatch(typeof(FloatMenuMakerMap), "TryMakeFloatMenu")]
-        static class Patch_TryMakeFloatMenu
+        private static class Patch_TryMakeFloatMenu
         {
             [HarmonyPriority(Priority.First)]
             static bool Prefix(Pawn pawn)
@@ -37,7 +33,7 @@ namespace DSGUI
         }
         
         [HarmonyPatch(typeof(GridsUtility), "GetThingList")]
-        public static class Patch_GetThingList
+        private static class Patch_GetThingList
         {
             public static bool Prepare(Harmony instance) 
             {
@@ -57,7 +53,7 @@ namespace DSGUI
         }
         
         [HarmonyPatch(typeof(GridsUtility), "GetFirstItem")]
-        public static class Patch_GetFirstItem
+        private static class Patch_GetFirstItem
         {
             public static bool Prepare(Harmony instance) 
             {
@@ -75,15 +71,22 @@ namespace DSGUI
                 return false;
             }
         }
-        
-        public static class Patch_AddHumanlikeOrders
-        {
-            public static MethodInfo getFI = AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetFirstItem));
-            public static MethodInfo getTL = AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetThingList));
-            
-            public static MethodInfo dsguiFI = AccessTools.Method(typeof(DSGUI), nameof(DSGUI.GetFirstItem));
-            public static MethodInfo dsguiTL = AccessTools.Method(typeof(DSGUI), nameof(DSGUI.GetThingList));
 
+        [HarmonyPatch(typeof(FloatMenuMakerMap), "AddHumanlikeOrders")]
+        private static class Patch_AddHumanlikeOrders
+        {
+            private static readonly MethodInfo getFI = AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetFirstItem));
+            private static readonly MethodInfo getTL = AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetThingList));
+
+            private static readonly MethodInfo dsguiFI = AccessTools.Method(typeof(DSGUI), nameof(DSGUI.GetFirstItem));
+            private static readonly MethodInfo dsguiTL = AccessTools.Method(typeof(DSGUI), nameof(DSGUI.GetThingList));
+            
+            public static bool Prepare(Harmony instance) 
+            {
+                return DSGUIMod.settings.DSGUI_UseTranspiler;
+            }
+
+            [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 var instructionList = instructions.ToList();
