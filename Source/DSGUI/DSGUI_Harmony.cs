4801302960296d214421a66c06ebbfac8929e325
Kristian Schlikow <kristian@schlikow.de>
2020-06-03 21:01:47 +0200

    Tanspiler working, with bugs though

--- Source/DSGUI/DSGUI_Harmony.cs
+++ Source/DSGUI/DSGUI_Harmony.cs
@@ -11,14 +11,14 @@ using Verse;
 
 namespace DSGUI
 {
-    public class HarmonyHelper
+    public static class HarmonyHelper
     {
-        public T ThingAt<T>(Map map, IntVec3 c) where T : Thing
+        public static T ThingAt<T>(ThingGrid thingGrid, IntVec3 c) where T : Thing
         {
             if (GlobalStorage.currThing != null && GlobalStorage.currThing is T)
                 return GlobalStorage.currThing as T;
             
-            return map.thingGrid.ThingAt<T>(c);
+            return thingGrid.ThingAt<T>(c);
         }
     }
     
@@ -42,28 +42,6 @@ namespace DSGUI
                 return DSGUI.Create(UI.MouseMapPosition(), pawn);
             }
         }
-        
-        /*
-        // Thanks to Garthor, Krafs, and Mehni for helping with this
-        [HarmonyPatch]
-        private static class Patch_ThingAt
-        {
-            private static MethodBase TargetMethod()
-            {
-                return AccessTools.Method(typeof(ThingGrid), "ThingAt", new[] {typeof(IntVec3)}).MakeGenericMethod(typeof(Apparel));
-            }
-
-            [HarmonyPriority(Priority.First)]
-            public static bool Prefix(ref Apparel __result, IntVec3 c)
-            {
-                if (GlobalStorage.currThing == null || !(GlobalStorage.currThing is Apparel apparel)) return true;
-                
-                __result = apparel; 
-                
-                return false;
-            }
-        }
-        */
 
         [HarmonyPatch(typeof(GridsUtility), "GetThingList")]
         private static class Patch_GetThingList
@@ -94,42 +72,18 @@ namespace DSGUI
                 return false;
             }
         }
-        
+
         [HarmonyPatch(typeof(FloatMenuMakerMap), "AddHumanlikeOrders")]
         private static class Patch_AddHumanlikeOrders
         {
-            private static readonly FieldInfo thingGrid = AccessTools.Field(typeof(Map), "thingGrid");
             private static readonly MethodInfo thingAt = AccessTools.Method(typeof(ThingGrid), "ThingAt", new[] {typeof(IntVec3)}).MakeGenericMethod(typeof(Apparel));
-            private static readonly MethodInfo dsguiThingAt = AccessTools.Method(typeof(HarmonyHelper), "ThingAt").MakeGenericMethod(typeof(Apparel));
+            private static readonly MethodInfo dsguiThingAt = AccessTools.Method(typeof(HarmonyHelper), "ThingAt", new[] {typeof(ThingGrid), typeof(IntVec3)}).MakeGenericMethod(typeof(Apparel));
             
             [HarmonyDebug]
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                var instructionList = instructions.ToList();
-
-                foreach (var instruction in instructionList)
-                {
-                    if (instruction.LoadsField(thingGrid))
-                    {
-                        instruction.opcode = OpCodes.Nop;
-                        instruction.operand = null;
-                        yield return instruction;
-                        
-                        continue;
-                    }
-                    
-                    if (instruction.Calls(thingAt))
-                    {
-                        instruction.opcode = OpCodes.Call;
-                        instruction.operand = dsguiThingAt;
-                        yield return instruction;
-                        
-                        continue;
-                    }
-
-                    yield return instruction;
-                }
+                return instructions.MethodReplacer(thingAt, dsguiThingAt);
             }
         }
     }
