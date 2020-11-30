4396363ca7e1fef84a6735d3a11f1c6d76552d0b
Doug Benham <dougbenham@cox.net>
2020-11-29 19:42:11 -0700

    Works better without even considering the rect

--- Source/DSGUI/Util/DSGUI_Elements.cs
+++ Source/DSGUI/Util/DSGUI_Elements.cs
@@ -103,7 +103,7 @@ namespace DSGUI
                 _tempGuiContent.text = buff; 
                 _doTextFieldMethod.Invoke(null, new object[]
                 {
-                    rect, GUIUtility.GetControlID(name.GetHashCode(), FocusType.Keyboard, rect), _tempGuiContent, false, max, Text.CurTextFieldStyle
+                    rect, GUIUtility.GetControlID(name.GetHashCode(), FocusType.Keyboard), _tempGuiContent, false, max, Text.CurTextFieldStyle
                 });
 			    buff = _tempGuiContent.text;
 
